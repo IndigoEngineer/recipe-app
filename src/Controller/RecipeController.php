6546287b54a2e4483871e64a0159337e29eab894
Dany <indigoEngineer@github.com>
2023-04-10 21:23:03 +0000

    adding crud for recipe

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Entity\Recipe;
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'recipe.index',methods: ['GET'])]
+    public function index(RecipeRepository $repository,PaginatorInterface $paginator, Request $request): Response
+    {
+        $recipes = $paginator->paginate(
+            $repository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1),/*page number*/
+            10 /*limit per page*/
+        );
+        return $this->render('pages/recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+            'recipes'=> $recipes
+        ]);
+    }
+
+    #[Route('/recipe/new', name: 'recipe.new',methods: ['GET','POST'])]
+    public function new(Request $request, EntityManagerInterface $manager) : Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest( $request) ;
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $recipe = $form->getData();
+            $manager->persist($recipe);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre recette have been created with success'
+            );
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render("pages/recipe/new.html.twig",[
+            'form'=>$form->createView()
+        ]);
+    }
+
+    #[Route('/recipe/edit/{id}', 'recipe.edit' , methods: ['GET','POST'])]
+    public function edit(recipe $recipe,
+                         Request $request,
+                         EntityManagerInterface $manager
+    ): Response {
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+//            dd($form->getData());
+            $recipe = $form->getData();
+            $manager->persist($recipe);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre recette a été modifié avec success'
+            );
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return  $this->render('pages/recipe/edit.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/recipe/delete/{id}', 'recipe.delete' , methods: ['GET'])]
+    public function delete(recipe $recipe,
+                           EntityManagerInterface $manager
+    ):response{
+        $manager->remove($recipe);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            'Votre recette a été supprimé avec success'
+        );
+        return $this->redirectToRoute('recipe.index');
+    }
+
+}
+
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Recipe;
 use Faker\Factory;
 use Faker\Generator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -18,15 +19,34 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        $ingredients = [];
         for($i = 0; $i < 50; $i++){
             $ingredient = new Ingredient();
-            $ingredient->setName($this->faker->words(3,true));
+            $ingredient->setName($this->faker->word());
             $ingredient->setPrice(mt_rand(1, 100));
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
 
         // $product = new Product();
+        // recipe
+        for($i = 0; $i < 25; $i++){
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->words(2,true));
+            $recipe->setDescription($this->faker->text(150));
+            $recipe->setDifficulty(mt_rand(1, 6));
 
+
+            $recipe->setPrice(mt_rand(1, 100))
+                   ->setTime(mt_rand(0,1) == 1 ? mt_rand(1,1441) : null)
+                   ->setNbPeople(mt_rand(0,1) == 1 ? mt_rand(1,49) : null)
+                   ->setIsFavourite(mt_rand(0,1) == 1);
+
+            for ($k = 0; $k < mt_rand(5, 15); $k++){
+                $recipe->addIngredient($ingredients[mt_rand(0,count($ingredients) - 1)]);
+            }
+            $manager->persist($recipe);
+        }
 
         $manager->flush();
     }
